9e888eaa3a6c2bd050ebe99f70274cc8700e0a45
Harshini-priyaa <harshinipriyaa1112@gmail.com>
2024-09-15 22:02:43 +0530

    footer

---
+++ uber/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react'
+
+export class Footer extends PureComponent {
+  render() {
+    return (
+      <div>
+        This creates footer
+      </div>
+    )
+  }
+}
+
+export default Footer
